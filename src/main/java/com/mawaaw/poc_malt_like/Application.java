1ef411af37c43341d44ae144e6787bc84af9fedb
MAWAAW <16020356+MAWAAW@users.noreply.github.com>
2024-11-21 00:59:14 +0100

    First commit

---
+++ src/main/java/com/mawaaw/poc_malt_like/Application.java
@@ -0,0 +1,13 @@
+package com.mawaaw.poc_malt_like;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/mawaaw/poc_malt_like/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.mawaaw.poc_malt_like;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
